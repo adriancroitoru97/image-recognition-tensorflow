c76af70be063f2eb413811cffaeea73ecace6cc3
Adrian Croitoru <adrian.croitoru.97@gmail.com>
2022-04-03 01:04:04 +0300

    afafaf

--- scripts.js
+++ scripts.js
@@ -7,37 +7,38 @@
 // Use this file to add JavaScript to your project
 
 
+
 /* click button scroll down script */
 window.smoothScroll = function(target) {
-    var scrollContainer = target;
-    do { //find scroll container
-        scrollContainer = scrollContainer.parentNode;
-        if (!scrollContainer) return;
-        scrollContainer.scrollTop += 1;
-    } while (scrollContainer.scrollTop == 0);
+  var scrollContainer = target;
+  do { //find scroll container
+      scrollContainer = scrollContainer.parentNode;
+      if (!scrollContainer) return;
+      scrollContainer.scrollTop += 1;
+  } while (scrollContainer.scrollTop == 0);
 
-    var targetY = 0;
-    do { //find the top of target relatively to the container
-        if (target == scrollContainer) break;
-        targetY += target.offsetTop;
-    } while (target = target.offsetParent);
+  var targetY = 0;
+  do { //find the top of target relatively to the container
+      if (target == scrollContainer) break;
+      targetY += target.offsetTop;
+  } while (target = target.offsetParent);
 
-    scroll = function(c, a, b, i) {
-        i++; if (i > 30) return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 20);
-    }
-    // start scrolling
-    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+  scroll = function(c, a, b, i) {
+      i++; if (i > 30) return;
+      c.scrollTop = a + (b - a) / 30 * i;
+      setTimeout(function(){ scroll(c, a, b, i); }, 20);
+  }
+  // start scrolling
+  scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
+
 
 
 
 /* loading spinning shit */
 var points = [],
-    velocity2 = 5, // velocity squared
-    canvas = 
-document.getElementById('container'),
+  velocity2 = 5, // velocity squared
+  canvas = document.getElementById('loading_container'),
   context = canvas.getContext('2d'),
   radius = 5,
   boundaryX = 200,
